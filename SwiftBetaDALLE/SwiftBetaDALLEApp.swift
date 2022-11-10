51a45b06729b0fc61495cc4ca10c03c7488010da
SwiftBeta <swiftbeta.blog@gmail.com>
2022-11-10 17:27:02 +0100

    Initial Commit

---
+++ SwiftBetaDALLE/SwiftBetaDALLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBetaDALLEApp.swift
+//  SwiftBetaDALLE
+//
+//  Created by Home on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBetaDALLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
